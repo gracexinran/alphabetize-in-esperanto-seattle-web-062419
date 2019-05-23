a45eadb83f22b4ddacadd5700de626f449768889
Xinran Che <gracexinran@gmail.com>
2019-05-23 23:24:10 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,53 +1,9 @@
 def alphabetize(arr)
   # code here
-  sorted = {}
-  english = 'abcdefghijklmnopqrstuvwxyz'.split
-  special = []
-  ALPHABET.split.each do |letter|
-    if !english.include?(letter)
-      special << letter
+  arr.sort_by do |x|
+    x.split.collect do |c|
+      ALPHABET.index(c)
     end
   end
-
-  arr.each do |element|
-    index = ALPHABET.index(element[0])
-    index_2 = ALPHABET.index(element.delete(' ')[1])
-    index_6 = ALPHABET.index(element.delete(' ')[5])
-    result = []
-    result_s = []
-    element.delete(' ').split.each do |e|
-      if special.include?(e)
-        result << e
-      end
-    end
-    index_s = ALPHABET.index(result[1])
-    if !sorted[index]
-      sorted[index] = [element]
-    else
-      if index_2 < ALPHABET.index(sorted[index].delete(' ')[1])
-        sorted[index].unshift(element)
-      elsif index_2 > ALPHABET.index(sorted[index].delete(' ')[1])
-        sorted[index].push(element)
-      else
-        if index_6 < ALPHABET.index(sorted[index].delete(' ')[5])
-          sorted[index].unshift(element)
-        elsif index_6 > ALPHABET.index(sorted[index].delete(' ')[5])
-          sorted[index].push(element)
-        else
-          sorted[index].delete(' ').split.each do |e|
-            if special.include?(e)
-              result_s << e
-            end
-          end
-          if index_s < ALPHABET.index(result_s[1])
-            sorted[index].unshift(element)
-          elsif index_s > ALPHABET.index(result_s[1])
-            sorted[index].push(element)
-          end
-        end
-      end
-    end
-  end
-
-  Hash[hash.sort].values
+  arr
 end
