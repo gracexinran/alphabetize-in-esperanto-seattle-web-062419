5ba80abaec1fd5e3cf1d0eff9d42490fa98570ec
Xinran Che <gracexinran@gmail.com>
2019-05-23 23:27:01 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   # code here
   a = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |x|
-    x.split("").collect do |c|
+    x.split("").map do |c|
       a.index(c)
     end
   end
