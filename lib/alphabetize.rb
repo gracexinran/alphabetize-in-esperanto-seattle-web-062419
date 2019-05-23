3876b2adbb648d01a0cde3f8ebf53c7f6ad24c66
Xinran Che <gracexinran@gmail.com>
2019-05-23 21:47:22 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
 def alphabetize(arr)
   # code here
   sort = {}
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
   arr.each do |element|
-    index = ESPERANTO_ALPHABET.index(element[0])
+    index = ALPHABET.index(element[0])
     sort[index] = element
   end
   sort.sort_by {|k,v| k}
