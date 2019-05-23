def8a7ce4d7045a9fe8767abf93495bc34ea8db7
Xinran Che <gracexinran@gmail.com>
2019-05-23 21:46:16 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
   # code here
+  sort = {}
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.each do |element|
+    index = ESPERANTO_ALPHABET.index(element[0])
+    sort[index] = element
+  end
+  sort.sort_by {|k,v| k}
 end
