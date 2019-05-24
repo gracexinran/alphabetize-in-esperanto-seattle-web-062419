6e0d82dd4a48c5c9bfae74cad1dc83033b007617
Xinran Che <gracexinran@gmail.com>
2019-05-24 00:02:11 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,40 +1,18 @@
 def alphabetize(arr)
   # code here
   # a = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
-  #
+  # 
   # arr.sort_by do |x|
-  #
+  # 
   #   x.split.sort_by do |c|
   #     a.index(c)
   #   end
   # end
-  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-esperanto_array = esperanto_alphabet.split("")
-final_array = []
-final_element = nil
-
-
-
-   arr.sort
-
-
-
-   arr.sort_by do |element|
-     #arr is array of sentences
-     #element is individual sentences
-
-     #holds an array of all letters in sentence
-     sentence_letters = []
-
-
-     sentence_letters = element.split("")
-     #sentence_letters is an array of each letter/space in the sentence
-
-     sentence_letters.sort_by do |individual_letters|
-     esperanto_array.index(individual_letters)
-
-
-     end
-   end
-
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", 
+    "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+    arr.sort_by do |word|
+      word.split('').collect do |letter|
+        esperanto_alphabet.index(letter)
+      end
+    end
 end
